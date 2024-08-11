60bddf3d8282998aa165faa332ba658cbe60b563
qac62 <qac62@Takumis>
2024-08-11 23:44:04 +0900

    作成１

--- src/product1/Main.java
+++ src/product1/Main.java
@@ -1,10 +1,20 @@
 package product1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		try {
+			System.out.print("Tell me your name! : ");
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String str = br.readLine();
+			System.out.println("Hello! Nice to meet you : " + str);
+		} catch (IOException e) {
+			System.out.println("何らかのエラーが発生しました。");
+		}
 	}
 
 }
